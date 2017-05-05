c4bfcc9f365c0f651a05d2bf17689ccb8aa21124
a86346480 <a86346480@gmail.com>
2017-05-04 22:41:19 -0400

    finish add book feature

---
+++ library10346007/app/controllers/candidates_controller.rb
@@ -0,0 +1,26 @@
+class CandidatesController < ApplicationController
+  def index
+    @candidates = Candidate.all
+  end
+  
+  def new
+    @candidate = Candidate.new
+  end
+  
+  def create
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      redirect_to candidates_path, notice: "新增書本成功!"
+    else
+      render :new
+    end
+  end
+  
+  
+  
+  private
+    def candidate_params
+      params.require(:candidate).permit(:title, :introduction, :author, :price)
+    end
+end
