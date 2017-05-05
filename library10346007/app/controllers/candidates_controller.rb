4ddd9f34f1b81590be6a81e64208a454ea3d9778
a86346480 <a86346480@gmail.com>
2017-05-04 22:54:46 -0400

    finish edit book feature

--- library10346007/app/controllers/candidates_controller.rb
+++ library10346007/app/controllers/candidates_controller.rb
@@ -17,6 +17,20 @@ class CandidatesController < ApplicationController
     end
   end
   
+  def edit
+    @candidate = Candidate.find_by(id: params[:id])
+  end
+  
+  def update
+    @candidate = Candidate.find_by(id: params[:id])
+
+    if @candidate.update(candidate_params)
+      redirect_to candidates_path, notice: "資料更新成功!"
+    else
+      render :edit
+    end
+  end
+  
   
   
   private
