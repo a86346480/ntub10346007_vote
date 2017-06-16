972462ad07de0c8290b31d7224801388e8cf4a15
a86346480 <a86346480@gmail.com>
2017-06-16 01:11:13 -0400

    finish list

---
+++ finalexam_07/db/migrate/20170616045236_create_candidates.rb
@@ -0,0 +1,11 @@
+class CreateCandidates < ActiveRecord::Migration[5.0]
+  def change
+    create_table :candidates do |t|
+      t.text :name
+      t.text :stu_number
+      t.integer :score
+
+      t.timestamps
+    end
+  end
+end
